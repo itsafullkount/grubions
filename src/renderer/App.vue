8768093403c62d86159452c1e93510f54f4aac03
ivan <itsafullkount@gmail.com>
2018-02-04 12:38:52 -0500

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'grubions'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.use(require('vue-pusher'), {
+  api_key: '8d8c103f64693bb64a39',
+  options: {
+    cluster: 'ap1',
+    encrypted: true
+  }
+})
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
